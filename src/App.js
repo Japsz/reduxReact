98ac116790693495932c9e3717f469276816f949
benjamin meneses <benjamin.meneses.14@sansano.usm.cl>
2019-07-23 10:24:02 -0400

    Ya no existe filteredLis.jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import store from './store/store';
 import Form from './views/Form/Form';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import List from './views/List/List';
-import FilteredList from "./views/List/FilteredList";
 
 import Header from "./common/layout/Header";
 import {Jumbotron} from "react-bootstrap";
@@ -21,7 +20,6 @@ function App() {
               <Switch>
                 <Route exact path={'/'} component={List}/>
                 <Route exact path={'/AddElement'} component={Form}/>
-                <Route path={'/filter/:key/:text'} component={FilteredList}/>
               </Switch>
             </Jumbotron>
         </Router>
